75dce1b9fc351b946ee8bda831672cd066994380
srahuliitb <srahul.iitb@gmail.com>
2024-05-10 17:18:32 +0100

    Added concrete implementation for insert and update methods in the CustomerDAOImplementation class

--- src/main/java/org/music/application/database/management/customer/CustomerDAOImplementation.java
+++ src/main/java/org/music/application/database/management/customer/CustomerDAOImplementation.java
@@ -8,6 +8,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class CustomerDAOImplementation implements CustomerDAO {
@@ -44,12 +45,49 @@ public class CustomerDAOImplementation implements CustomerDAO {
 
             customer = new Customer(customerId, firstName, lastName, company, address, city, state, country, postalCode, phone, fax, email, supportRepId);
         }
+
+        Database.closePreparedStatement(prepStatement);
+        Database.closeConnection(conn);
+
         return customer;
     }
 
+    /**
+     * Retrieves all customers from the database.
+     *
+     * @return A list containing all customer objects retrieved from the database.
+     * @throws SQLException If an SQL exception occurs while accessing the database.
+     */
     @Override
     public List<Customer> getAll() throws SQLException {
-        return null;
+        Connection conn = Database.getConnection();
+        List<Customer> customers = new ArrayList<>();
+        String sqlQuery = "SELECT * FROM Customer";
+        PreparedStatement prepStatement = conn.prepareStatement(sqlQuery);
+        ResultSet resultSet = prepStatement.executeQuery();
+        while (resultSet.next()) {
+            int customerId = resultSet.getInt("CustomerId");
+            String firstName = resultSet.getString("FirstName");
+            String lastName = resultSet.getString("LastName");
+            String company = resultSet.getString("Company");
+            String address = resultSet.getString("Address");
+            String city = resultSet.getString("City");
+            String state = resultSet.getString("State");
+            String country = resultSet.getString("Country");
+            String postalCode = resultSet.getString("PostalCode");
+            String phone = resultSet.getString("Phone");
+            String fax = resultSet.getString("Fax");
+            String email = resultSet.getString("Email");
+            int supportRepId = resultSet.getInt("SupportRepId");
+
+            Customer customer = new Customer(customerId, firstName, lastName, company, address, city, state, country, postalCode, phone, fax, email, supportRepId);
+            customers.add(customer);
+        }
+
+        Database.closePreparedStatement(prepStatement);
+        Database.closeConnection(conn);
+
+        return customers;
     }
 
     @Override
@@ -57,14 +95,74 @@ public class CustomerDAOImplementation implements CustomerDAO {
         return 0;
     }
 
+    /**
+     * Inserts a new customer into the database.
+     *
+     * @param customer The customer object to insert into the database.
+     * @return The number of rows affected by the insert operation.
+     * @throws SQLException If an SQL exception occurs while accessing the database.
+     */
     @Override
     public int insert(Customer customer) throws SQLException {
-        return 0;
+        Connection conn = Database.getConnection();
+        String sqlQuery = "INSERT INTO Customer (CustomerId, FirstName, LastName, Company, Address, City, State, Country, PostalCode, Phone, Fax, Email, SupportRepId) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)";
+        PreparedStatement prepStatement = conn.prepareStatement(sqlQuery);
+
+        prepStatement.setInt(1, customer.getCustomerId());
+        prepStatement.setString(2, customer.getFirstName());
+        prepStatement.setString(3, customer.getLastName());
+        prepStatement.setString(4, customer.getCompany());
+        prepStatement.setString(5, customer.getAddress());
+        prepStatement.setString(6, customer.getCity());
+        prepStatement.setString(7, customer.getState());
+        prepStatement.setString(8, customer.getCountry());
+        prepStatement.setString(9, customer.getPostalCode());
+        prepStatement.setString(10, customer.getPhone());
+        prepStatement.setString(11, customer.getFax());
+        prepStatement.setString(12, customer.getEmail());
+        prepStatement.setInt(13, customer.getSupportRepId());
+
+        int result = prepStatement.executeUpdate();
+
+        Database.closePreparedStatement(prepStatement);
+        Database.closeConnection(conn);
+
+        return result;
     }
 
+    /**
+     * Updates an existing customer in the database with the provided customer information.
+     *
+     * @param customer The updated customer object containing new information.
+     * @return The number of rows affected by the update operation.
+     * @throws SQLException If an SQL exception occurs while accessing the database.
+     */
     @Override
     public int update(Customer customer) throws SQLException {
-        return 0;
+        Connection conn = Database.getConnection();
+        String sqlQuery = "UPDATE Customer SET FirstName = ?, LastName = ?, Company = ?, Address = ?, City = ?, State = ?, Country = ?, PostalCode = ?, Phone = ?, Fax = ?, Email = ?, SupportRepId = ? WHERE CustomerId = ?";
+        PreparedStatement prepStatement = conn.prepareStatement(sqlQuery);
+
+        prepStatement.setString(1, customer.getFirstName());
+        prepStatement.setString(2, customer.getLastName());
+        prepStatement.setString(3, customer.getCompany());
+        prepStatement.setString(4, customer.getAddress());
+        prepStatement.setString(5, customer.getCity());
+        prepStatement.setString(6, customer.getState());
+        prepStatement.setString(7, customer.getCountry());
+        prepStatement.setString(8, customer.getPostalCode());
+        prepStatement.setString(9, customer.getPhone());
+        prepStatement.setString(10, customer.getFax());
+        prepStatement.setString(11, customer.getEmail());
+        prepStatement.setInt(12, customer.getSupportRepId());
+        prepStatement.setInt(13, customer.getCustomerId());
+
+        int result = prepStatement.executeUpdate();
+
+        Database.closePreparedStatement(prepStatement);
+        Database.closeConnection(conn);
+
+        return result;
     }
 
     @Override
--- src/main/java/org/music/application/database/management/customer/Main.java
+++ src/main/java/org/music/application/database/management/customer/Main.java
@@ -3,23 +3,37 @@ package org.music.application.database.management.customer;
 import javax.xml.crypto.Data;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) throws SQLException {
-//        Customer customer = new Customer(10, "Eduardo", "Martins",
-//                "Woodstock Discos", "Rua Dr. Falcão Filho, 155", "São Paulo",
-//                "SP", "Brazil", "01007-010", "+55 (11) 3033-5446",
-//                "+55 (11) 3033-4564", "eduardo@woodstock.com.br", 4);
-//
-//        System.out.println(customer);
 //        try (Connection conn = Database.getConnection()) {
 //            System.out.println("Database connection successful!");
 //        } catch (Exception e) {
 //            e.printStackTrace();
 //        }
         CustomerDAO customerDAO = new CustomerDAOImplementation();
-        Customer customer = customerDAO.get(5);
-        System.out.println(customer);
+//        Customer customer = customerDAO.get(5);
+//        System.out.println(customer);
+
+        List<Customer> customers = customerDAO.getAll();
+        for (Customer c : customers) {
+            System.out.println(c);
+        }
+
+//        Customer customer = new Customer(60, "Rayna", "Zhao",
+//                "Newcastle University", "Vita Strawberry", "Newcastle upon Tyne",
+//                "Tyne and Wear", "UK", "NE1 4PQ", "+44 020 3096 1717",
+//                null, "Zhao.Rayna@ncl.co.uk", 4);
+//        int result = customerDAO.insert(customer);
+//        System.out.println(result);
+
+//        Customer customer = new Customer(60, "Ruirui", "Zhao",
+//                "School of Computing, Newcastle University", "Vita Strawberry", "Newcastle upon Tyne",
+//                "Tyne and Wear", "UK", "NE1 4PQ", "+44 020 3096 1717",
+//                null, "Zhao.Rayna@ncl.co.uk", 4);
+//        int result = customerDAO.update(customer);
+//        System.out.println(result);
     }
 }
