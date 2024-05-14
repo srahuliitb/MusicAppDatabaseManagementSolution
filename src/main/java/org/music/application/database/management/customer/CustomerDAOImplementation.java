2fd4737552c181312f7c4b7ce4c173bd9785183f
srahuliitb <srahul.iitb@gmail.com>
2024-05-14 16:20:38 +0100

    Added functionalities for the delete and save methods

--- src/main/java/org/music/application/database/management/customer/CustomerDAOImplementation.java
+++ src/main/java/org/music/application/database/management/customer/CustomerDAOImplementation.java
@@ -12,7 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CustomerDAOImplementation implements CustomerDAO {
-    // CRUD - Retrieve
+    // CRUD - Retrieve one
     /**
      * Retrieves a customer from the database based on the provided customer ID.
      *
@@ -52,6 +52,7 @@ public class CustomerDAOImplementation implements CustomerDAO {
         return customer;
     }
 
+    // CRUD - Retrieve all
     /**
      * Retrieves all customers from the database.
      *
@@ -90,11 +91,7 @@ public class CustomerDAOImplementation implements CustomerDAO {
         return customers;
     }
 
-    @Override
-    public int save(Customer customer) throws SQLException {
-        return 0;
-    }
-
+    // CRUD - Create
     /**
      * Inserts a new customer into the database.
      *
@@ -130,6 +127,7 @@ public class CustomerDAOImplementation implements CustomerDAO {
         return result;
     }
 
+    // CRUD - Update
     /**
      * Updates an existing customer in the database with the provided customer information.
      *
@@ -165,8 +163,92 @@ public class CustomerDAOImplementation implements CustomerDAO {
         return result;
     }
 
+    // CRUD - Delete
+    /**
+     * Deletes the specified customer from the database.
+     *
+     * @param customer The customer object to delete.
+     * @return The number of rows affected by the delete operation.
+     * @throws RuntimeException If an SQL exception occurs while accessing the database.
+     */
     @Override
     public int delete(Customer customer) {
-        return 0;
+        try {
+            Connection conn = Database.getConnection();
+            String sqlQuery = "DELETE FROM Customer WHERE CustomerId = ?";
+            PreparedStatement prepStatement = conn.prepareStatement(sqlQuery);
+            prepStatement.setInt(1, customer.getCustomerId());
+            int result = prepStatement.executeUpdate();
+
+            Database.closePreparedStatement(prepStatement);
+            Database.closeConnection(conn);
+
+            return result;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    // CRUD - Create or update
+    /**
+     * Saves a customer by performing either a create or update operation.
+     * If the customer object has a valid CustomerId, it updates the existing record in the database.
+     * If the customer object does not have a valid CustomerId, it creates a new record in the database.
+     *
+     * @param customer The customer object to save.
+     * @return The number of rows affected. If the customer object was newly created, 1 is returned. If it was updated, the number of updated rows is returned.
+     * @throws RuntimeException If a runtime exception occurs while accessing the database.
+     */
+    @Override
+    public int save(Customer customer) {
+        try {
+            Connection conn = Database.getConnection();
+            int result;
+            if (customer.getCustomerId() > 0) {
+                // Perform an update operation
+                String sqlUpdateQuery = "UPDATE Customer SET FirstName = ?, LastName = ?, Company = ?, Address = ?, City = ?, State = ?, Country = ?, PostalCode = ?, Phone = ?, Fax = ?, Email = ?, SupportRepId = ? WHERE CustomerId = ?";
+                PreparedStatement updateStatement = conn.prepareStatement(sqlUpdateQuery);
+                updateStatement.setString(1, customer.getFirstName());
+                updateStatement.setString(2, customer.getLastName());
+                updateStatement.setString(3, customer.getCompany());
+                updateStatement.setString(4, customer.getAddress());
+                updateStatement.setString(5, customer.getCity());
+                updateStatement.setString(6, customer.getState());
+                updateStatement.setString(7, customer.getCountry());
+                updateStatement.setString(8, customer.getPostalCode());
+                updateStatement.setString(9, customer.getPhone());
+                updateStatement.setString(10, customer.getFax());
+                updateStatement.setString(11, customer.getEmail());
+                updateStatement.setInt(12, customer.getSupportRepId());
+                updateStatement.setInt(13, customer.getCustomerId());
+
+                result = updateStatement.executeUpdate();
+                Database.closePreparedStatement(updateStatement);
+            } else {
+                // Perform a create operation
+                String sqlInsertQuery = "INSERT INTO Customer (CustomerId, FirstName, LastName, Company, Address, City, State, Country, PostalCode, Phone, Fax, Email, SupportRepId) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)";
+                PreparedStatement insertStatement = conn.prepareStatement(sqlInsertQuery);
+                insertStatement.setInt(1, customer.getCustomerId());
+                insertStatement.setString(2, customer.getFirstName());
+                insertStatement.setString(3, customer.getLastName());
+                insertStatement.setString(4, customer.getCompany());
+                insertStatement.setString(5, customer.getAddress());
+                insertStatement.setString(6, customer.getCity());
+                insertStatement.setString(7, customer.getState());
+                insertStatement.setString(8, customer.getCountry());
+                insertStatement.setString(9, customer.getPostalCode());
+                insertStatement.setString(10, customer.getPhone());
+                insertStatement.setString(11, customer.getFax());
+                insertStatement.setString(12, customer.getEmail());
+                insertStatement.setInt(13, customer.getSupportRepId());
+
+                result = insertStatement.executeUpdate();
+                Database.closePreparedStatement(insertStatement);
+            }
+            Database.closeConnection(conn);
+            return result;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/main/java/org/music/application/database/management/customer/Main.java
+++ src/main/java/org/music/application/database/management/customer/Main.java
@@ -13,27 +13,41 @@ public class Main {
 //        } catch (Exception e) {
 //            e.printStackTrace();
 //        }
+
         CustomerDAO customerDAO = new CustomerDAOImplementation();
 //        Customer customer = customerDAO.get(5);
 //        System.out.println(customer);
 
-        List<Customer> customers = customerDAO.getAll();
-        for (Customer c : customers) {
-            System.out.println(c);
-        }
+//        List<Customer> customers = customerDAO.getAll();
+//        for (Customer c : customers) {
+//            System.out.println(c);
+//        }
 
-//        Customer customer = new Customer(60, "Rayna", "Zhao",
+//        Customer customer = new Customer(60, "Songyun", "Tan",
 //                "Newcastle University", "Vita Strawberry", "Newcastle upon Tyne",
 //                "Tyne and Wear", "UK", "NE1 4PQ", "+44 020 3096 1717",
-//                null, "Zhao.Rayna@ncl.co.uk", 4);
+//                null, "Tan.Songyun@ncl.co.uk", 4);
 //        int result = customerDAO.insert(customer);
 //        System.out.println(result);
 
-//        Customer customer = new Customer(60, "Ruirui", "Zhao",
+//        Customer customer = new Customer(60, "Songyun", "Tan",
 //                "School of Computing, Newcastle University", "Vita Strawberry", "Newcastle upon Tyne",
 //                "Tyne and Wear", "UK", "NE1 4PQ", "+44 020 3096 1717",
-//                null, "Zhao.Rayna@ncl.co.uk", 4);
+//                null, "Tan.Songyun@ncl.co.uk", 4);
 //        int result = customerDAO.update(customer);
+//        System.out.println(result);
+
+//        Customer customer = customerDAO.get(0);
+//        System.out.println(customer);
+//
+//        int result = customerDAO.delete(customer);
+//        System.out.println(result);
+
+//        Customer customer = new Customer(60, "Songyun", "Tan",
+//                "Newcastle University", "Vita Strawberry", "Newcastle upon Tyne",
+//                "Tyne and Wear", "UK", "NE1 4PQ", "+44 020 3096 1717",
+//                null, "Tan.Songyun@ncl.co.uk", 4);
+//        int result = customerDAO.save(customer);
 //        System.out.println(result);
     }
 }
